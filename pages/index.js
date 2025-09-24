169e8d3398c2b3ab90728b22fe29665a70a86506
Jmd-1993 <Josh@skinsociete.com.au>
2025-09-24 06:06:12 -0700

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Skin Societe Booking API</h1>
+      <p>This repository hosts the serverless API for Skin Societe booking flow.</p>
+    </div>
+  );
+}
