50a2c1162358fbcbce4299adae4785268e51dc40
Jmd-1993 <Josh@skinsociete.com.au>
2025-09-24 06:07:12 -0700

    Create _phorest.js

---
+++ pages/api/_phorest.js
@@ -0,0 +1,20 @@
+const BASE = "https://api-gateway-eu.phorest.com/third-party-api-server/api";
+
+function getAuthHeaders() {
+  const username = process.env.PHOREST_USER;
+  const password = process.env.PHOREST_PASS;
+  const token = Buffer.from(`${username}:${password}`).toString('base64');
+  return { Authorization: `Basic ${token}` };
+}
+
+const BUSINESS_ID = process.env.PHOREST_BUSINESS_ID;
+const COTTESLOE_BRANCH = "wQbnBjP6ztI8nuVpNT6MsQ";
+const SKIN_NEEDLING_SERVICE_COTT = "z5dmn_xMisQXNyFNbu4ktw";
+
+module.exports = {
+  BASE,
+  getAuthHeaders,
+  BUSINESS_ID,
+  COTTESLOE_BRANCH,
+  SKIN_NEEDLING_SERVICE_COTT
+};
